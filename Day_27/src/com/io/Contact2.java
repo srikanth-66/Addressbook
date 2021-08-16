0a4134117e5c72b8573995abff7c4f6b431cb4a6
Srikanth_M <srikanthMidathapalli@gmail.com>
2021-08-16 15:46:05 +0530

    contact2 addressbook commit

---
+++ Day_27/src/com/io/Contact2.java
@@ -0,0 +1,75 @@
+package com.io;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Contact2 {
+	public static Map<String, Object> map = new HashMap<String, Object>();
+	public static List<String> names=new ArrayList<String>();
+
+	private String FullName;
+	private String address;
+	private String cityName;
+	private String stateName;
+	private String zipCode;
+	private String phoneNumber;
+	private String emailAddress;
+
+	public String getFullName() {
+		return FullName;
+	}
+
+	public void setFullName(String fullName) {
+		FullName = fullName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+
+	public String getStateName() {
+		return stateName;
+	}
+
+	public void setStateName(String stateName) {
+		this.stateName = stateName;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+}
